afde1c0ed1b1027f1f116cbb0b07619d92a574ce
Abel Acosta <abel.2833@hotmail.com>
2023-06-06 22:15:52 -0500

    Modulo ventas

---
+++ Modules/SalesManagment/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('salesmanagment::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('salesmanagment.name') !!}
+    </p>
+@endsection
